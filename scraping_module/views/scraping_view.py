8a07658a77902cb3ea840467ff3da88fa02a802a
kato0905 <lookup373@yahoo.co.jp>
2018-08-04 15:20:30 +0900

    update scraping_view

--- scraping_module/views/scraping_view.py
+++ scraping_module/views/scraping_view.py
@@ -7,6 +7,120 @@ import re
 import datetime
 import logging
 
+#from .scraping_view import list1, data_sex, data_handicap, data_odds, data_popularity, data_past_achievement,
+
+#レース情報までの入れ子
+#   1 レース一覧ページのトップページ
+#       2 特定の日付ごとのレース一覧ページ
+#           3 個別のレースページ
+#               4 馬情報ページや調教師情報ページ
+
+
+def find_initialize():
+    #URLを受け取って、適切な形に直したりする
+    url = 'http://race.netkeiba.com/?pid=race_list' #レース一覧ページのリンク
+    racedate_url = [] #
+    soup = bs4.BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser") #レース一覧ページをsoupで取得
+    for i in range(0, len(soup.find('div', class_='DateList_Box').find_all('a'))):
+        racedate_url.append(soup.find('div', class_='DateList_Box')('a')[i].get('href', None))#racedate_urlに特定の日付のレース一覧のURLを配列形式で保存
+    return racedate_url
+
+
+def race_link_conversion(racedate_url):#特定の日付のレース一覧を受け取り、個別のレースのリンクの一覧として返す
+    url = "http://race.netkeiba.com" + racedate_url  # URL
+    race_list = bs4.BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")  # 2 特定の日付ごとのレース一覧ページ
+    get = race_list.find_all('div', class_="racename")  # 個別のレースのリンクの一覧(使える形に修正する前)
+    individual_race_list = []
+    for i in range(0, len(get)):  # イニシャルページからレースへのaタグを取る
+        individual_race_url = get[i].find('a')
+        individual_race_url = "http://race.netkeiba.com" + individual_race_url.get('href', None)
+        individual_race_list.append(individual_race_url)  # 個別のレースのリンクの一覧(修正後)
+    return individual_race_list
+
+
+def find_horse_data(individual_race_url):
+    #horse_name, horse_birth, horse_link
+    #全て配列
+    horse_name = []
+    horse_link = []
+    horse_birth = []
+    url = individual_race_url
+    individual_race_contents = bs4.BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")  #個別レースページを展開
+    horselink = individual_race_contents.find_all('a', href=re.compile('http://db.netkeiba.com/horse/'))
+    for j in range(0, len(horselink)):
+        horse_name.append(horselink[j].get('title'))
+        horselink[j] = horselink[j].get('href', None)
+        horse_link.append(horselink[j])
+        horselink[j] = bs4.BeautifulSoup(urllib.request.urlopen(horselink[j]).read(), "html.parser")
+        table = horselink[j].find('table', class_='db_prof_table')
+        row = table.find('tr')
+        horse_birth.append(row.find('td').string)
+    return 0
+
+
+def find_jockey_data(individual_race_url):
+    #jockey_name, jockey_link
+    #全て配列
+    url = individual_race_url
+    individual_race_contents = bs4.BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")#2 特定の日付ごとのレース一覧ページ
+    jockey_name = []
+    jockey_link = []
+    jockeylink = individual_race_contents.find_all('a', href=re.compile('http://db.netkeiba.com/jockey/'))
+    for j in range(0, len(jockeylink)):  # ジョッキーページごと
+        jockey_name.append(jockeylink[j].get('title'))
+        jockeylink[j] = jockeylink[j].get('href', None)
+        jockey_link.append(jockeylink[j])
+    return 0
+
+
+def find_trainer_data(individual_race_url):
+    #trainer_name, trainer_link
+    #全て配列
+    trainer_name = []
+    trainer_link = []
+    url = individual_race_url  # URL
+    individual_race_contents = bs4.BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")#2 特定の日付ごとのレース一覧ページ
+    trainerlink = individual_race_contents.find_all('a', href=re.compile('http://db.netkeiba.com/trainer/'))
+    for j in range(0, len(trainerlink)):  # 馬ページごと
+        trainer_name.append(trainerlink[j].get('title'))
+        trainerlink[j] = trainerlink[j].get('href', None)
+        trainer_link.append(trainerlink[j])
+    return 0
+
+
+def find_stable_data(individual_race_url):
+    #stable_name, stable_link
+    #stable_nameは配列、stable_linkは使われていない
+    stable_name = []
+    stable_link = []
+    url = individual_race_url  # URL
+    individual_race_contents = bs4.BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")#2 特定の日付ごとのレース一覧ページ
+    trainerlink = individual_race_contents.find_all('a', href=re.compile('http://db.netkeiba.com/trainer/'))
+    for j in range(0, len(trainerlink)):  # 馬ページごと
+        stable_name.append(trainerlink[j].get('title'))
+    return 0
+
+
+def find_race_data(individual_race_url):
+    #race_number, race_name, race_arena, groundcondition_value, course_value, distance_value, race_head_count
+    url = individual_race_url  # URL
+    individual_race_contents = bs4.BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")#2 特定の日付ごとのレース一覧ページ
+    raceval = individual_race_contents.find('div', class_='mainrace_data')
+    courseval = raceval.find('p').string
+    courseval2 = raceval('p')[1].string
+    course_value = courseval[0]
+    distance_value = courseval[1:-3]
+    groundcondition_value = re.compile('馬場：.*/').search(courseval2).string
+    race_date = individual_race_contents.find('title').string[0:11]
+    raceval2 = individual_race_contents.find('title').string.split(" ")
+    race_number = raceval2[2]
+    race_name = raceval2[3]
+    race_arena = raceval2[1]
+    horselink = individual_race_contents.find_all('a', href=re.compile('http://db.netkeiba.com/horse/'))
+    race_head_count = len(horselink)
+    return 0
+
+
 conn = sqlite3.connect('db.sqlite3')
 c = conn.cursor()
 
@@ -124,17 +238,9 @@ for so in range(0,len(ddate)):#一日ごとにスクレイピング
                         data_past_achievement.append(table2val[l])
 
 
-
-    # horselink = bs4.BeautifulSoup(urllib.request.urlopen(racelink[i]).read())
-   # horselist.insert()
-
-#c.execute('insert into umauma_happy_app_horse(id, name, birth_year, link, created_at, updated_at) values (2,?,2,3,4,5)',(horsename,))
-#もう終わったレース
-#str(data_rank)+str(data_horse_order)+str(data_horse_id)+str(data_popularity)+str(data_odds)
-#まだ始まっていないレース
-#+str(list1)+str(data_sex)+str(data_handicap)+str(data_odds)+str(data_popularity)+str(data_past_achievement)+str(race_date)+str(race_number)+str(race_name)+str(race_arena)+str(race_head_count)+str(groundcondition_value)+str(course_value)+str(distance_value)+str(jockey_name)+str(jockey_link)+str(trainer_name)+str(horse_name)+str(horse_birth)+str(horse_link)
 conn.commit()
 
+
 def index(request):
-    return HttpResponse(str(ddate))
+    return HttpResponse("flasd")
 
